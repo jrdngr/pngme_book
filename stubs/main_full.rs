e03884aa98af88208f1ff1ae96d430bea9d14aab
Jordan Grace <jordan.t.grace@gmail.com>
2020-06-08 20:43:28 -0400

    Added book files to root

---
+++ stubs/main_full.rs
@@ -0,0 +1,12 @@
+mod args;
+mod chunk;
+mod chunk_type;
+mod commands;
+mod png;
+
+pub type Error = Box<dyn std::error::Error>;
+pub type Result<T> = std::result::Result<T, Error>;
+
+fn main() -> Result<()> {
+    todo!()
+}
